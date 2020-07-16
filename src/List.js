9c911ad710f7b6444a3ad33778f9147609fae199
Tkachenko Vladislav <webkadiz@gmail.com>
2020-07-16 19:35:32 +0300

    Add implementation List

---
+++ src/List.js
@@ -0,0 +1,45 @@
+module.exports = class List {
+  constructor(initVal) {
+    if (Array.isArray(initVal)) this._list = initVal
+    else if (initVal === undefined) this._list = []
+    else throw new Error()
+  }
+
+  add(item) {
+    this._list.push(item)
+    return this
+  }
+
+  clear() {
+    this._list = []
+    return this
+  }
+
+  remove(item) {
+    const itemArr = this._list.filter(_item => _item !== item)
+    this._list = itemArr
+    return this
+  }
+
+  get(item) {
+    const itemArr = this._list.filter(_item => _item === item)
+    
+    if (itemArr.length) return itemArr
+    else return false
+  }
+
+  getOne(item) {
+    const returnedItems = this.get(item)
+
+    if (returnedItems) return returnedItems[0]
+    else return false
+  }
+
+  getAll() {
+    return this._list
+  }
+
+  length() {
+    return this._list.length
+  }
+}
